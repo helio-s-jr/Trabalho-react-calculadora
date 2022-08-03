73356406b579b73096258a4a05f2356d34f6bb71
Helio Junior <heliojr.zpt@gmail.com>
2022-08-03 16:00:34 -0300

    Projeto de calculadora em react em andamento

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component{
+
+    state = {
+        numero1: "",
+        numero2: "",
+        resultado: ""
+    }
+
+    adicao = () => {
+        this.setState({
+            resultado: Number(this.state.numero1) + Number(this.state.numero2)
+        })
+    }
+
+    subtracao = () => {
+        this.setState({
+            resultado: Number(this.state.numero1) - Number(this.state.numero2)
+        })
+    }
+
+    multiplicacao = () => {
+        this.setState({
+            resultado: Number(this.state.numero1) * Number(this.state.numero2)
+        })
+    }
+
+    divisao = () => {
+        this.setState({
+            resultado: Number(this.state.numero1) / Number(this.state.numero2)
+        })
+    }
+
+    limpar = () => {
+        this.setState({
+            resultado: "",
+            numero1: "",
+            numero2: ""
+        })
+    }
+
+    handleChange1 = (event) => {
+        this.setState({
+            numero1: event.target.value
+        })
+    }
+
+    handleChange2 = (event) => {
+        this.setState({
+            numero2: event.target.value
+        })
+    }
+
+    render(){
+        return(
+            <Calculadora>
+                <h1>Calculadora</h1>
+                <div>
+                    <input type="number" value={this.state.numero1} onChange={this.handleChange1}/>
+                    <input type="number" value={this.state.numero2} onChange={this.handleChange2}/>
+                </div>
+                <section>
+                    <button onClick={this.adicao}>+</button>
+                    <button onClick={this.subtracao}>-</button>
+                    <button onClick={this.multiplicacao}>x</button>
+                    <button onClick={this.divisao}>÷</button>
+                    <button onClick={this.limpar}>c</button>
+                </section>
+                <h2>{this.state.resultado}</h2>
+            </Calculadora>
+        )
+    }
 }
-
-export default App;
